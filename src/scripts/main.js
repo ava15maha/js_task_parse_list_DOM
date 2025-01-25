20bf64206401df09bed8e77afdc61b509d1929b6
Maria <m.abeeva.ma@gmail.com>
2025-01-25 18:39:31 +0300

    Fixed Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,10 +1,20 @@
 'use strict';
 
 function getSalary(salary) {
-  return +salary.replace(/[^0-9]/g, '');
+  if (!salary) {
+    return 0;
+  }
+
+  const salaryToNum = salary.replace(/[^0-9]/g, '');
+
+  return salaryToNum ? +salaryToNum : 0;
 }
 
 function sortList(list) {
+  if (!(list instanceof HTMLElement)) {
+    return;
+  }
+
   const items = Array.from(list.querySelectorAll('li'));
 
   const itemsSorted = items.sort((a, b) => {
@@ -20,13 +30,25 @@ function sortList(list) {
 }
 
 function getEmployees(list) {
+  if (!(list instanceof HTMLElement)) {
+    return;
+  }
+
   const employees = Array.from(list.querySelectorAll('li'));
 
   return employees.map((el) => {
-    const names = el.textContent.trim();
+    // eslint-disable-next-line no-shadow
+    const name = el.textContent.trim();
+    const position = el.dataset.position;
     const salary = getSalary(el.getAttribute('data-salary'));
+    const age = el.dataset.age;
 
-    return { names, salary };
+    return {
+      name,
+      position,
+      salary,
+      age,
+    };
   });
 }
 
